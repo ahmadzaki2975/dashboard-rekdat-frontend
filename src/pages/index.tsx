5bd2393beb5bbec84f26695f396ec23b91f11fc4
Ahmad Zaki Akmal <87590846+ahmadzaki2975@users.noreply.github.com>
2023-11-29 22:11:03 +0700

    feat: allow custom inputs

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,6 @@
+import CustomTooltip from "@/components/CustomTooltip";
 import axios from "axios";
+import dynamic from "next/dynamic";
 import { useEffect, useState } from "react";
 import {
   LineChart,
@@ -10,39 +12,67 @@ import {
   Tooltip,
 } from "recharts";
 
-export default function Home() {
+function Home() {
   const [data, setData] = useState([]);
-  const [edate, setEdate] = useState("");
-  const [sdate, setSdate] = useState("");
-  const [city, setCity] = useState("");
+  const [sdate, setSdate] = useState("2023-11-20");
+  const [edate, setEdate] = useState("2023-11-21");
+  const [city, setCity] = useState("Sleman");
+  const [refetch, setRefetch] = useState(false);
+
+  const FormatDate = (date: string) => {
+    // format to yyyy-mm-dd
+    let d = new Date(date);
+    let month = "" + (d.getMonth() + 1);
+    let day = "" + d.getDate();
+    let year = d.getFullYear();
+
+    return [year, month, day].join("-");
+  };
 
   useEffect(() => {
     axios
       .get(
         process.env.NEXT_PUBLIC_API_URL +
-          "/join?sdate=2023-11-19&edate=2023-11-20&city=Sleman"
+          `/join?sdate=${sdate}&edate=${edate}&city=${city}`
       )
       .then((res) => {
         setData(res.data);
         console.log(res.data);
+        if(res.data.length === 0) alert("No Data");
       })
       .catch((err) => {
         console.log(err);
+        alert("Error");
       });
-  }, []);
+  }, [refetch]);
   return (
-    <main className="bg-white min-h-screen flex flex-col justify-center items-center font-poppins">
+    <main className="bg-white min-h-screen flex flex-col justify-start py-10 items-center font-poppins">
       <h1 className="font-bold text-[25px]">
         Weather and Air Quality Dashboard
       </h1>
 
-      <form className="grid grid-cols-3 gap-5">
+      <form
+        className="grid grid-cols-4 gap-5 mt-5"
+        onSubmit={(e) => {
+          e.preventDefault();
+          if(sdate > edate) return alert("Start Date must be before End Date");
+          if(city === "") {
+            setCity("Sleman");
+            alert("City must be filled")
+          };
+          setRefetch(!refetch);
+        }}
+      >
         <label className="flex flex-col gap-1">
           Start Date
           <input
             type="date"
             className="outline-1 outline rounded-[5px] px-2 py-1"
-            onChange={(e) => setSdate(e.target.value)}
+            value={sdate}
+            onChange={(e) => {
+              setSdate(FormatDate(e.target.value));
+              console.log(FormatDate(e.target.value));
+            }}
           />
         </label>
         <label className="flex flex-col gap-1">
@@ -50,7 +80,11 @@ export default function Home() {
           <input
             type="date"
             className="outline-1 outline rounded-[5px] px-2 py-1"
-            onChange={(e) => setSdate(e.target.value)}
+            value={edate}
+            onChange={(e) => {
+              setEdate(FormatDate(e.target.value));
+              console.log(FormatDate(e.target.value));
+            }}
           />
         </label>
         <label className="flex flex-col gap-1">
@@ -58,23 +92,29 @@ export default function Home() {
           <input
             type="text"
             className="outline-1 outline rounded-[5px] px-2 py-1"
-            onChange={(e) => setSdate(e.target.value)}
+            value={city}
+            onChange={(e) => setCity(e.target.value)}
           />
         </label>
+        <button className="bg-green-400 rounded-[5px]" type="submit">
+          Set Input
+        </button>
       </form>
 
-      <div>
+      <div className="mt-10">
         <LineChart width={1000} height={500} data={data}>
           <XAxis dataKey="jamcuaca" />
           <YAxis />
           <CartesianGrid stroke="#eee" strokeDasharray="5 5" />
           <Line type="monotone" dataKey="iqa" stroke="#8884d8" />
           <Line type="monotone" dataKey="tempc" stroke="#82ca9d" />
-          <Line type="monotone" dataKey="humidity" stroke="#82ca9d" />
+          <Line type="monotone" dataKey="humidity" stroke="#DAA520" />
           {/* <Line type="monotone" dataKey="pressure_mbar" stroke="#82ca9d" /> */}
-          <Tooltip />
+          <Tooltip content={<CustomTooltip />} />
         </LineChart>
       </div>
     </main>
   );
 }
+
+export default dynamic(() => Promise.resolve(Home), { ssr: false });
