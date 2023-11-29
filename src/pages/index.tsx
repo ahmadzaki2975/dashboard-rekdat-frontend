fcc2476c36205f780707d02a60ff8ff3db5b6838
Ahmad Zaki Akmal <87590846+ahmadzaki2975@users.noreply.github.com>
2023-11-29 21:16:37 +0700

    feat: add inputs

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,13 +1,26 @@
 import axios from "axios";
 import { useEffect, useState } from "react";
-import { LineChart, Line, XAxis, YAxis, CartesianGrid } from "recharts";
+import {
+  LineChart,
+  Line,
+  XAxis,
+  YAxis,
+  CartesianGrid,
+  Legend,
+  Tooltip,
+} from "recharts";
 
 export default function Home() {
   const [data, setData] = useState([]);
+  const [edate, setEdate] = useState("");
+  const [sdate, setSdate] = useState("");
+  const [city, setCity] = useState("");
+
   useEffect(() => {
     axios
       .get(
-        process.env.NEXT_PUBLIC_API_URL + "/bmkg?sdate=2023-11-19&edate=2023-11-20&city=Sleman"
+        process.env.NEXT_PUBLIC_API_URL +
+          "/join?sdate=2023-11-19&edate=2023-11-20&city=Sleman"
       )
       .then((res) => {
         setData(res.data);
@@ -22,13 +35,46 @@ export default function Home() {
       <h1 className="font-bold text-[25px]">
         Weather and Air Quality Dashboard
       </h1>
-      {/* <LineChart width={500} height={300} data={[1, 2, 3]}>
-        <XAxis dataKey="name" />
-        <YAxis />
-        <CartesianGrid stroke="#eee" strokeDasharray="5 5" />
-        <Line type="monotone" dataKey="uv" stroke="#8884d8" />
-        <Line type="monotone" dataKey="pv" stroke="#82ca9d" />
-      </LineChart> */}
+
+      <form className="grid grid-cols-3 gap-5">
+        <label className="flex flex-col gap-1">
+          Start Date
+          <input
+            type="date"
+            className="outline-1 outline rounded-[5px] px-2 py-1"
+            onChange={(e) => setSdate(e.target.value)}
+          />
+        </label>
+        <label className="flex flex-col gap-1">
+          End Date
+          <input
+            type="date"
+            className="outline-1 outline rounded-[5px] px-2 py-1"
+            onChange={(e) => setSdate(e.target.value)}
+          />
+        </label>
+        <label className="flex flex-col gap-1">
+          City
+          <input
+            type="text"
+            className="outline-1 outline rounded-[5px] px-2 py-1"
+            onChange={(e) => setSdate(e.target.value)}
+          />
+        </label>
+      </form>
+
+      <div>
+        <LineChart width={1000} height={500} data={data}>
+          <XAxis dataKey="jamcuaca" />
+          <YAxis />
+          <CartesianGrid stroke="#eee" strokeDasharray="5 5" />
+          <Line type="monotone" dataKey="iqa" stroke="#8884d8" />
+          <Line type="monotone" dataKey="tempc" stroke="#82ca9d" />
+          <Line type="monotone" dataKey="humidity" stroke="#82ca9d" />
+          {/* <Line type="monotone" dataKey="pressure_mbar" stroke="#82ca9d" /> */}
+          <Tooltip />
+        </LineChart>
+      </div>
     </main>
   );
 }
